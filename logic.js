1dd767d02b2cb13488539d06b5c5e237d5329e97
Krivko <xkrivosikm1@stuba.sk>
2022-12-29 14:36:46 +0100

    quesitons.json - changed argQ, franQ,... to argentina, france,... added player/enemy start coinflip rnd question from enemy shows modal when right answer

--- logic.js
+++ logic.js
@@ -4,9 +4,18 @@ var teams = await fetch('../json/questions.json').then(r=>r.json())
 
 
 
-// Game logic
+////////////////////////////// GAME LOGIC //////////////////////////////
+
+//TODO connect to sliders on setup page
+let playerTeam = "argentina"
+let enemyTeam = "france"
+let difficulty = "easy"
+let turn = Math.random() >= 0.5 ? "player" : "enemy";
+let playerNumber = 0;
+let enemyNumber = 0;
+
+
 const question = document.getElementById("questionText")
-question.textContent = players.argentina[0].easy
 const answerA = document.getElementById("answerA")
 const answerB = document.getElementById("answerB")
 const answerC = document.getElementById("answerC")
@@ -14,48 +23,120 @@ const answerD = document.getElementById("answerD")
 
 const modal = new bootstrap.Modal( document.getElementById("exampleModal"))
 
-let shuffledArray = players.argentina[0].optionse.sort((a, b) => 0.5 - Math.random());
+let index = 0
+if (turn == "player"){
+	question.textContent = players[playerTeam][playerNumber][difficulty]
+}
+else{
+	index = Math.floor(Math.random() * teams[enemyTeam].length)
+	let randomQuestion = teams[enemyTeam][index]
+	question.textContent = randomQuestion.question
+}
+
+let options;
+let answer;
+if (difficulty == "easy"){
+    options = "optionse"
+    answer = "answere"
+}
+else{
+    options = "optionsh"
+    answer = "answerh"
+}
+
+let shuffledArray;
+if (turn == "player"){
+	shuffledArray = players[playerTeam][playerNumber][options].sort((a, b) => 0.5 - Math.random());
+}
+else{
+	shuffledArray = teams[enemyTeam][index].options.sort((a, b) => 0.5 - Math.random()) 
+}
 
 answerA.textContent = shuffledArray[0]
 answerB.textContent = shuffledArray[1]
 answerC.textContent = shuffledArray[2]
 answerD.textContent = shuffledArray[3]
 
+
 answerA.addEventListener("click", function(){
-    if(answerA.textContent == players.argentina[0].answere){
-        answerA.style.background = "green"
-        modal.show()
-    }
-    else{
-        answerA.style.background = "red"
-    }
+	if (turn == "player"){
+		if(answerA.textContent == players[playerTeam][playerNumber][answer]){
+			answerA.style.background = "green"
+			modal.show()
+		}
+		else{
+			answerA.style.background = "red"
+		}
+	}
+	else{
+		if(answerA.textContent == teams[enemyTeam][index].answer){
+			answerA.style.background = "green"
+			modal.show()
+		}
+		else{
+			answerA.style.background = "red"
+		}
+	}
 })
 answerB.addEventListener("click", function(){
-    if(answerB.textContent == players.argentina[0].answere){
-        answerB.style.background = "green"
-        modal.show()
-    }
-    else{
-        answerB.style.background = "red"    
-    }
+    if (turn == "player"){
+		if(answerB.textContent == players[playerTeam][playerNumber][answer]){
+			answerB.style.background = "green"
+			modal.show()
+		}
+		else{
+			answerB.style.background = "red"
+		}
+	}
+	else{
+		if(answerB.textContent == teams[enemyTeam][index].answer){
+			answerB.style.background = "green"
+			modal.show()
+		}
+		else{
+			answerB.style.background = "red"
+		}
+	}
 })
 answerC.addEventListener("click", function(){
-    if(answerC.textContent == players.argentina[0].answere){
-        answerC.style.background = "green"
-        modal.show()
-    }
-    else{
-        answerC.style.background = "red"
-    }
+    if (turn == "player"){
+		if(answerC.textContent == players[playerTeam][playerNumber][answer]){
+			answerC.style.background = "green"
+			modal.show()
+		}
+		else{
+			answerC.style.background = "red"
+		}
+	}
+	else{
+		if(answerC.textContent == teams[enemyTeam][index].answer){
+			answerC.style.background = "green"
+			modal.show()
+		}
+		else{
+			answerC.style.background = "red"
+		}
+	}
 })
 answerD.addEventListener("click", function(){
-    if(answerD.textContent == players.argentina[0].answere){
-        answerD.style.background = "green"
-        modal.show()
-    }
-    else{
-        answerD.style.background = "red"
-    }
+    if (turn == "player"){
+		if(answerD.textContent == players[playerTeam][playerNumber][answer]){
+			answerD.style.background = "green"
+			modal.show()
+		}
+		else{
+			answerD.style.background = "red"
+		}
+	}
+	else{
+		if(answerD.textContent == teams[enemyTeam][index].answer){
+			answerD.style.background = "green"
+			modal.show()
+		}
+		else{
+			answerD.style.background = "red"
+		}
+	}
 })
 
-console.log(teams)
+////////////////////////////////////////////////////////////////////////////////
